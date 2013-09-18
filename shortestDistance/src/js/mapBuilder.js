29b552164414edee767160ac215376b4474d5cfe
Miike Ramos <mikeramos@gmail.com>
2013-09-17 19:02:53 -0700

    Fixed some bugs with my Dijkstra's algorithm

--- shortestDistance/src/js/mapBuilder.js
+++ shortestDistance/src/js/mapBuilder.js
@@ -2,11 +2,13 @@ var param = window.location.hash;
 var input = param.replace('#', '') || 15;
 var canvasWidth = 800;
 var canvasHeight = 800;
+var radius = 500;//400;//340;
 var cities;
 var graph = {};
 var selectOutput = '';
 
 var save = '[{"city":8,"x":134,"y":123,"fromOrigin":181.89282558693733},{"city":7,"x":118,"y":430,"fromOrigin":445.896849058165},{"city":4,"x":45,"y":494,"fromOrigin":496.0453608290274},{"city":3,"x":500,"y":64,"fromOrigin":504.0793588315237},{"city":2,"x":452,"y":240,"fromOrigin":511.7655713312493},{"city":9,"x":615,"y":177,"fromOrigin":639.9640614909559},{"city":5,"x":240,"y":636,"fromOrigin":679.7764338368902},{"city":0,"x":127,"y":745,"fromOrigin":755.7473122677976},{"city":6,"x":760,"y":238,"fromOrigin":796.3943746662203},{"city":1,"x":648,"y":508,"fromOrigin":823.3881223335687}]';
+var simple = '[{"city":0,"x":25,"y":300,"fromOrigin":301.0398644698074},{"city":1,"x":200,"y":100,"fromOrigin":223.60679774997897},{"city":2,"x":80,"y":600,"fromOrigin":605.3098380168623},{"city":3,"x":400,"y":320,"fromOrigin":512.2499389946279},{"city":4,"x":420,"y":620,"fromOrigin":748.8658090739622},{"city":5,"x":550,"y":100,"fromOrigin":559.0169943749474},{"city":6,"x":750,"y":400,"fromOrigin":960.4686356149273}]';
 
 var Dijkstra = function() {
   this.nodes = {};
@@ -38,7 +40,7 @@ var Dijkstra = function() {
   };
 
   this.findPath = function() {
-    var city, distance;
+    var city, distance, check;
     var roads = this.currentNode.roads;
     
     // Base case:
@@ -46,16 +48,25 @@ var Dijkstra = function() {
       // 6. Trace your path back to the start node by subtracting distances
       console.log('reached the end, begin tracing backwards');
     } else {
-      // 1. Create nodes for each road connecting to a city and assing temporary, working values
+
+      debugger;
+      // 1. Create nodes for each road connecting to a city and assign temporary, working values. Only consider roads that haven't been given permanent labels.
       for (var i = 0; i < roads.length; i += 1) {
         city = roads[i][0];
         distance = roads[i][1];
-        // Assign working labels to each dijkstra city node
-        if (this.nodes[city]) {
-          this.nodes[city].working += distance;
-        } else {
-          // There was no node for this city so we create it and assign it the distance
-          this.addNode(city, null, null, distance);
+        // Only assess this city if it doesn't have a permanent label
+        check = this.nodes[city] && this.nodes[city].order;
+        if (!check) {
+          // Assign working labels to each dijkstra city node
+          if (this.nodes[city]) {
+            // If the cumulative distance on the current node + the distance down this road is less than the working value stored there, set it. If not do nothing
+            if ((this.nodes[this.currentNode.city].working + distance) < this.nodes[city].working) {
+              this.nodes[city].working += distance;
+            }
+          } else {
+            // There was no node for this city so we create it and assign it the distance
+            this.addNode(city, null, null, this.nodes[this.currentNode.city].distance + distance);
+          }
         }
       }
       
@@ -65,7 +76,7 @@ var Dijkstra = function() {
         if (!this.nodes[key].order) {
           if (this.shortest.distance) {
             if (this.nodes[key].working < this.shortest.distance) {
-              this.shortest.distance = this.nodes[key].distance;
+              this.shortest.distance = this.nodes[key].working;
               this.shortest.city = key;
             }
           } else {
@@ -77,6 +88,8 @@ var Dijkstra = function() {
 
       // 3. Assign a permanent label of distance to the node with the shortest distance
       this.nodes[this.shortest.city].order = this.order += 1;
+      // Also assign the permanent distance
+      this.nodes[this.shortest.city].distance += this.shortest.distance;
 
       // 4. Move to the new node (assign it to the current) and wipe away the values in shortest
       // TODO: Unfortunately this requires a loop through the cities array (again) because the data it's saved in is an array. A hash table might serve better
@@ -90,7 +103,7 @@ var Dijkstra = function() {
       this.shortest = {};
 
       // 5. Call findPath again. The new parameters have all been set -- see base case above for how the function should end
-      debugger;
+      // debugger;
       this.findPath();
     }
   };
@@ -123,8 +136,8 @@ var plotRoads = function(cities) {
     for (var connectingCity in cities) {
       // Ensure we're not comparing the same city
       if (cities[city].city !== cities[connectingCity].city) {
-        // If the difference in distance from the origin to both points is <= to 340...
-        if (Math.abs(cities[city].fromOrigin - cities[connectingCity].fromOrigin) <= 340) {
+        // If the difference in distance from the origin to both points is <= to radius...
+        if (Math.abs(cities[city].fromOrigin - cities[connectingCity].fromOrigin) <= radius) {
           // Calculate the actual distance between the two points
           var distance = Math.sqrt(
             Math.pow(
@@ -134,8 +147,8 @@ var plotRoads = function(cities) {
               (cities[connectingCity].y - cities[city].y), 2
             )
           );
-          // If the actual distance is within 340 then add it
-          if (distance <= 340) {
+          // If the actual distance is within radius then add it
+          if (distance <= radius) {
             // Comparative option for testing
             connectRoad(cities[city], [cities[connectingCity].city, distance, cities[connectingCity].x, cities[connectingCity].y]);
             graph[cities[city].city][cities[connectingCity].city] = distance;
@@ -156,7 +169,8 @@ var plotRoads = function(cities) {
 
 // 1. Generate cities
 // cities = generateCities(input);
-cities = JSON.parse(save);
+// cities = JSON.parse(save);
+cities = JSON.parse(simple);
 
 // 2. Sort the cities array of objects by their distance from the origin
 cities.sort(function(a, b) {
@@ -169,8 +183,6 @@ cities.sort(function(a, b) {
 
 // 3. Plot roads (based on distance). This mutates the city objects embedded in cities
 plotRoads(cities);
-// console.log(cities);
-// console.log(graph);
 
 // 4. Draw the graph using canvas
 var canvas = document.getElementById('map-canvas');
@@ -207,26 +219,29 @@ var handler = function() {
   var from = document.getElementById('from').selectedIndex || 0;
   var to = document.getElementById('to').selectedIndex;
   var results = dijkstra.find_path(graph, from, to);
+  console.log('dijkstra results', results);
   var highlightLine = [];
 
-  var description = '<div>Starting from point ' + from + '</div>';
+  var description = '';//'<div>Starting from point ' + from + '</div>';
 
   // Clear out the output box
   document.getElementById('plotDetails').innerHTML = '';
 
   // Draw the highlight line by building a new object of the plot points
   // Need to grab the x,y coordinates of each point by looping through the cities array of objects
-  for (var j = 0; j < cities.length; j += 1) {
-    var city = cities[j].city;
-    // Loop through the results array ensures we only do this gleaning once
-    for (var k = 0; k < results.length; k += 1) {
-      if (city === parseInt(results[k]) && from === parseInt(results[k])) {
-        highlightLine[city] = {};
-        highlightLine[city].x = cities[j].x;
-        highlightLine[city].y = cities[j].y;
-      }
-    }
-  }
+  // console.log('cities', cities);
+  // for (var j = 0; j < cities.length; j += 1) {
+  //   var city = cities[j].city;
+  //   // Loop through the results array ensures we only do this gleaning once
+  //   for (var k = 0; k < results.length; k += 1) {
+  //     debugger;
+  //     if (city === parseInt(results[k]) || from === parseInt(results[k])) {
+  //       highlightLine[city] = {};
+  //       highlightLine[city].x = cities[j].x;
+  //       highlightLine[city].y = cities[j].y;
+  //     }
+  //   }
+  // }
   // console.log('highlightLine',highlightLine);
 
   // var highlightCanvas = document.getElementById('map-highlight');
@@ -245,7 +260,9 @@ var handler = function() {
   //   highlightContext.lineTo(highlightLine[results[i]].x, highlightLine[results[i]].y);
   //   highlightContext.strokeStyle = 'rgba(231,60,69,0.9)';
   //   highlightContext.stroke();
-    description += '<div>Move to ' + results[i] + '</div>';
+    if (parseInt(results[i]) !== from) {
+      description += '<div>Move to ' + results[i] + '</div>';
+    }
   }
   // console.log(results);
   document.getElementById('plotDetails').innerHTML = description;
@@ -260,8 +277,8 @@ el.addEventListener('click', handler, false);
 
 
 var dj = new Dijkstra();
-var start = cities[0];
-var end = cities[1];
+var start = cities[1];
+var end = cities[6];
 dj.plot(start, end);
 
 
