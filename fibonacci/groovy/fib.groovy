b3ba6529ef2c73ff94efba5777810ab9fad01a60
Miike <ticketflyuser@mr.local>
2013-10-09 16:25:29 -0700

    Fibonacci sequence in Groovy

---
+++ fibonacci/groovy/fib.groovy
@@ -0,0 +1,24 @@
+/*
+ * Print a number (n) of fibonacci numbers in a list
+ */
+
+def fib(n) {
+    def index = 2
+    def list = [0,1]
+    if (n == 1) {
+        println list[n]
+    } else if (n == 2) {
+        println list
+    } else {
+    	while (index < n) {
+        	list.push(list[index - 1] + list[index - 2])
+        	index += 1
+    	}
+    	println list
+    }
+}
+
+for (a in this.args) {
+	fib(a.toInteger())
+}
+
